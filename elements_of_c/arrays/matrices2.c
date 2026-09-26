18b8a7043f5b35d4cf029331e7143d0d2c9c962a
Abhishek M <abhi.md.official@gmail.com>
2026-09-26 19:26:44 +0530

    matrix problem is solved

--- elements_of_c/arrays/matrices2.c
+++ elements_of_c/arrays/matrices2.c
@@ -4,7 +4,7 @@
 #include<stdio.h>
 int main()
 {
-    int i, j, mat1[ROW][COL],mat2[ROW][COL],mat3[ROW][COL];
+    int i, j, k, mat1[ROW][COL],mat2[ROW][COL],mat3[ROW][COL];
     printf("Enter the array elements of the matrix 1: \n");
     for(i=0; i<ROW; i++)
     {
@@ -20,8 +20,19 @@ int main()
         printf("\n");
     }  
     for(i=0; i<ROW; i++)
-        {    printf("The resultant of two matrices: \n");      
-            for(j=0; j<COL; j++)
-                mat3[i][j]=mat1[i][j]+mat2[i][j];
-            printf("\n");
+        for(j=0; j<COL; j++)
+        {
+            mat3[i][j]=0;
+            for(k=0; k<COL; k++)
+            {
+                mat3[i][j] += mat1[i][k] * mat2[k][j];
+            }
         }
+    printf("The resultant of two matrices: \n");
+    for(i=0; i<ROW; i++)
+    {
+        for(j=0; j<COL; j++)
+            printf("%d\t", mat3[i][j]);
+        printf("\n");
+    }     
+}
